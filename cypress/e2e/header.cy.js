24dd1eaaa08dfe10df0e211f88c3b15daba66f00
efrencodes <efrencodes@gmail.com>
2023-07-04 20:09:01 -0600

    🎉 Begin a project

---
+++ cypress/e2e/header.cy.js
@@ -0,0 +1,16 @@
+describe("Probando headers", () => {
+  it("Validar el header y el content type", () => {
+    cy.request("/employees")
+      .its("headers")
+      .its("content-type")
+      .should("include", "application/json");
+  });
+  it("Validando el status correcto", () => {
+    cy.request("/employees").its("status").should("eq", 200);
+  });
+  it("Validando el status incorrecto", () => {
+    cy.request({ url: "/employees/12", failOnStatusCode: false })
+      .its("status")
+      .should("eq", 404);
+  });
+});
